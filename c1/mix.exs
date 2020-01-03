2576190c976a9cc4ebd858a8859bb4cc499f367c
Junyu Zhan <thezjy@gmail.com>
2020-01-03 15:33:23 +0800

    update absinthe related packages

--- c1/mix.exs
+++ c1/mix.exs
@@ -33,20 +33,21 @@ defmodule C1.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.11"},
-      {:phoenix_pubsub, "~> 1.1"},
-      {:phoenix_ecto, "~> 4.0"},
+      {:absinthe_phoenix, "~> 1.5.0-alpha.0"},
+      {:absinthe_plug, "~> 1.5.0-rc.0"},
+      {:absinthe_relay, "~> 1.5.0-rc.0"},
+      {:absinthe, "~> 1.5.0-rc.1"},
+      {:distillery, "~> 2.1"},
       {:ecto_sql, "~> 3.1"},
-      {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
+      {:phoenix_ecto, "~> 4.0"},
+      {:phoenix_html, "~> 2.11"},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_pubsub, "~> 1.1"},
+      {:phoenix, "~> 1.4.11"},
       {:plug_cowboy, "~> 2.0"},
-      {:absinthe, "~> 1.5.0-beta.2"},
-      {:absinthe_plug, "~> 1.5.0-alpha.0"},
-      {:absinthe_phoenix, "~> 1.5.0-alpha.0"},
-      {:distillery, "~> 2.1"}
+      {:postgrex, ">= 0.0.0"}
     ]
   end
 
